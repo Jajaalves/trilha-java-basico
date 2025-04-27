5d46b8791d54424676c233e4a8ee2953c8a5fb11
Jajaalves <jarjaalves@gmail.com>
2025-04-27 13:48:56 -0300

    Add files via upload

---
+++ tipoVariaveis.java
@@ -0,0 +1,28 @@
+public class tipoVariaveis {
+    public static void main(String[] args) throws Exception {
+        // Variáveis do tipo inteiro
+        int idade = 20;
+        int ano = 2023;
+
+        // Variáveis do tipo ponto flutuante
+        double altura = 1.75;
+        double peso = 70.5;
+
+        // Variáveis do tipo caractere
+        char letra = 'A';
+
+        // Variáveis do tipo booleano
+        boolean isEstudante = true;
+
+        // Exibindo os valores das variáveis
+       System.err.println("Idade: " + idade);
+        System.err.println("Ano: " + ano);
+        System.err.println("Altura: " + altura);
+        System.err.println("Peso: " + peso);
+        System.err.println("Letra: " + letra);
+        System.err.println("É estudante? " + isEstudante);
+
+        
+       
+    }
+}
